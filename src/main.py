fd1e586d1ec963da528ba57d6c1dc1dd4df467fb
marcosdevelo <marcosgomezda@gmail.com>
2019-09-11 23:20:40 +0000

    lkjasdf

--- src/main.py
+++ src/main.py
@@ -8,6 +8,9 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from models import db
+from models import db, User
+from models import db, Player
+from models import db, Teams
 #from models import Person
 
 app = Flask(__name__)
@@ -28,14 +31,79 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/hello', methods=['POST', 'GET'])
-def handle_person():
+@app.route('/user', methods=['GET'])
+def get_user():
+    all_people = User.query.all()
+    all_people_serialized = list(map(lambda x: x.serialize(), all_people))
 
-    response_body = {
-        "hello": "world"
-    }
 
-    return jsonify(response_body), 200
+    return jsonify(all_people_serialized), 200
+
+@app.route('/player', methods=['GET'])
+def get_player():
+    all_people = Player.query.all()
+    all_people_serialized = list(map(lambda x: x.serialize(), all_people))
+
+
+    return jsonify(all_people_serialized), 200
+
+@app.route('/teams', methods=['GET'])
+def get_team():
+    all_people = Teams.query.all()
+    all_people_serialized = list(map(lambda x: x.serialize(), all_people))
+
+
+    return jsonify(all_people_serialized), 200
+
+@app.route('/user', methods=['POST'])
+def post_persona():
+
+    body = request.get_json()
+
+    # if 'full_name' not in body:
+    #     raise APIException('Hermamanazo te equivocaste en el full_name', status_code=400)
+
+    # if body["email"].find('@') == -1:
+    #     raise APIException('Mamita te falta un arroba en el email', status_code=400)
+
+    user1 = User(full_name=body["full_name"], email=body["email"], screen_name=body["screen_name"], password=body["password"])
+    db.session.add(user1)
+    db.session.commit()
+
+    return jsonify(user1.serialize()), 200
+
+@app.route('/user/<int:id>', methods=['PUT'])
+def put_persona(id):
+
+    body = request.get_json()
+    user1 = User.query.get(id)
+    if user1 is None:
+        raise APIException('User not found', status_code=404)
+
+    if "full_name" in body:
+        user1.full_name = body["full_name"]
+    if "email" in body:
+        user1.email = body["email"]
+    if "phone" in body:
+        user1.screen_name = body["screen_name"]
+    if "password" in body:
+        user1.password = body["password"]
+    db.session.commit()
+
+    return jsonify(user1.serialize()), 200
+
+@app.route('/user/<int:id>', methods=['DELETE'])
+def delete_persona(id):
+
+    user1 = User.query.get(id)
+    if user1 is None:
+        raise APIException('User not found', status_code=404)
+    db.session.delete(user1)
+    db.session.commit()
+
+    return jsonify(user1.serialize()), 200
+
+
 
 # this only runs if `$ python src/main.py` is exercuted
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,16 +2,108 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# class Person(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
 
-#     def __repr__(self):
-#         return '<Person %r>' % self.username
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    screen_name = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(120), unique=True, nullable=False)
 
-#     def serialize(self):
-#         return {
-#             "username": self.username,
-#             "email": self.email
-#         }
+
+    def __repr__(self):
+        return '<Person %r>' % self.email
+
+    def serialize(self):
+        return {
+            "id":self.id,
+            "full_name": self.full_name,
+            "email": self.email,
+            "screen_name": self.screen_name,
+            "password": self.password,
+            }
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(80), unique=True, nullable=False)
+    team_id = db.Column(db.String(120), unique=True, nullable=False)
+    computer_team_id = db.Column(db.String(100), unique=True, nullable=False)
+
+
+
+    def __repr__(self):
+        return '<Person %r>' % self.computer_team_id
+
+    def serialize(self):
+        return {
+            "id":self.id,
+            "user_id": self.full_name,
+            "email": self.email,
+            "team_id": self.screen_name,
+            "computer_team_id": self.password,
+            }
+
+class Activity(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    slug = db.Column(db.String(80), unique=True, nullable=False)
+    game_id = db.Column(db.String(120), unique=True, nullable=False)
+
+
+
+    def __repr__(self):
+        return '<Person %r>' % self.slug
+
+    def serialize(self):
+        return {
+            "id":self.id,
+            "slug": self.slug,
+            "game_id": self.game_id,
+            }
+
+class Teams(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    team_id = db.Column(db.String(120), unique=True, nullable=False)
+    fixture_id = db.Column(db.String(120), unique=True, nullable=False)
+    activity_id = db.Column(db.String(120), unique=True, nullable=False)
+
+
+    def __repr__(self):
+        return '<Person %r>' % self.team_id
+
+    def serialize(self):
+        return {
+            "id":self.id,
+            "name": self.name,
+            "team_id": self.team_id,
+            "fixture_id": self.fixture_id,
+            "activity_id": self.activity_id,
+            }
+
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    team_id = db.Column(db.String(80), unique=True, nullable=False)
+    attack = db.Column(db.String(80), unique=True, nullable=False)
+    defense = db.Column(db.String(80), unique=True, nullable=False)
+    player_id = db.Column(db.String(120), unique=True, nullable=False)
+    team_goals = db.Column(db.String(100), unique=True, nullable=False)
+    team_passes = db.Column(db.String(120), unique=True, nullable=False)
+    team_fouls = db.Column(db.String(120), unique=True, nullable=False)
+    team_blocked_shots = db.Column(db.String(120), unique=True, nullable=False)
+
+
+    def __repr__(self):
+        return '<Person %r>' % self.attack
+
+    def serialize(self):
+        return {
+            "id":self.id,
+            "team_id":self.team_id,
+            "attack":self.attack,
+            "defense": self.defense,
+            "player_id": self.player_id,
+            "team_goals": self.team_goals,
+            "team_passes": self.team_passes,
+            "team_fouls": self.team_fouls,
+            "team_blocked_shots": self.team_blocked_shots,
+            }
